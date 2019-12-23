32df8ef820b62062b5680d9ce434dffbc077e9c5
Pasha Smolov <palexsmol@gmail.com>
2019-12-23 13:14:32 -0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { BASE_URL } from "./index";
 
 function App() {
+  const [count, setCount] = React.useState(0);
+  const [people, setPeople] = React.useState([]);
+
+  async function getPeople() {
+    const res = await fetch(`${BASE_URL}/?count=${count}`);
+    const resData = await res.json();
+    setPeople(resData.data);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <h1>React & Node</h1>
+          <input
+            style={{ fintSize: "2rem" }}
+            onChange={event => setCount(event.target.value)}
+            placeholder="Number of people."
+            type="number"
+          />
+          <button style={{ fontSize: "2rem" }} onClick={getPeople}>
+            Submit
+          </button>
+          {people.map(person => (
+            <div
+              key={person.email}
+              style={{
+                width: "100%",
+                display: "flex",
+                marginTop: ".5em",
+                justifyContent: "space-between",
+                alignCenter: "center"
+              }}
+            >
+              <img
+                style={{
+                  borderRadius: "50%",
+                  height: "100%"
+                }}
+                src={person.picture.large}
+                alt={person.name.first}
+              />
+              <div>
+                <h3>{person.name.first}</h3>
+                <p>{person.email}</p>
+              </div>
+            </div>
+          ))}
+        </div>
       </header>
     </div>
   );
